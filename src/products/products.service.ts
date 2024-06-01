eff4017ff81e43d6111d435c9ec7bbe37d7c26cf
R-Berrocal <robertoberro20@gmail.com>
2024-06-01 11:35:01 -0500

    insertar y comprobar la base de datos

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -11,7 +11,9 @@ export class ProductsService extends PrismaClient implements OnModuleInit {
     this.logger.log('Connected to database');
   }
   create(createProductDto: CreateProductDto) {
-    return 'This action adds a new product';
+    return this.product.create({
+      data: createProductDto,
+    });
   }
 
   findAll() {
